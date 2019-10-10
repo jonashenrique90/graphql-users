4ec986cad6c55e46aff687fd443ff15d7b1cea41
Jonas <jonas.herique90@gmail.com>
2019-10-10 15:04:47 -0300

    Adiciona server.js ao projeto

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ knexfile = require('../knexfile')
 const knex = require('knex')(knexfile)
 module.exports = knex
 
-// knex('users').insert({
-//     name: 'Teste',
-//     email: 'teste@teste.com.br',
-//     password: 'teste'
-// }).then(data => console.log(data))
+knex('users').insert({
+    name: 'Teste',
+    email: 'teste@teste.com.br',
+    password: 'teste'
+}).then(data => console.log(data))
 
-knex('users').select('*').then(resultado =>console.log(resultado))
+// knex('users').select('*').then(resultado =>console.log(resultado))
